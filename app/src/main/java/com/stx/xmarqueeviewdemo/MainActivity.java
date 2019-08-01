be287029dafb6c42328865e19357b9f35ecc6c92
Mr.xiao <xhb_199409@163.com>
2019-08-01 19:00:53 +0800

    优化demo

--- app/src/main/java/com/stx/xmarqueeviewdemo/MainActivity.java
+++ app/src/main/java/com/stx/xmarqueeviewdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.stx.xmarqueeviewdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.stx.xmarqueeview.XMarqueeView;
 
@@ -11,6 +13,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<String> mDatas;
+    private MarqueeViewAdapter mMarqueeViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +38,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         XMarqueeView xMarqueeView = (XMarqueeView) findViewById(R.id.marquee3);
-        xMarqueeView.setAdapter(new MarqueeViewAdapter(mDatas, this));
+        mMarqueeViewAdapter = new MarqueeViewAdapter(mDatas, this);
+        xMarqueeView.setAdapter(mMarqueeViewAdapter);
+        Button btn = (Button) findViewById(R.id.btn);
 
-        //刷新数据
-//        marqueeViewAdapter.setData(mDatas);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatas.clear();
+                for (int i = 0; i < 4; i++) {
+                    mDatas.add("这是刷新后的跑马灯内容" + (i + 1));
+                }
+                //刷新数据
+                mMarqueeViewAdapter.setData(mDatas);
+            }
+        });
     }
 
 }
